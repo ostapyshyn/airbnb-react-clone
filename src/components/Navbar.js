3509eb7e03334f52b160ecf834c289e02a346eec
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-12-06 20:27:44 +0200

    Finish project

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+// import logo from "../../public/images/airbnb-logo.png"
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src='images/airbnb-logo.png' className="nav--logo" />
+        </nav>
+    )
+}
